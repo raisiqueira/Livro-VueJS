8e39173f56e3d2037e1f04893778fd9b9f9af538
Rai Siqueira <rai93siqueira@gmail.com>
2017-05-17 15:15:04 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+      <my-menu title="Propriedade do Menu" v-on:menu-click="OnMenuClick">
+          <ul>
+              <li>Slot 1</li>
+              <li>Slot 2</li>
+              <li>Slot 3</li>
+          </ul>
+      </my-menu>
+  </div>
+</template>
+
+<script>
+  import MyMenu from './Mymenu.vue'
+
+export default {
+  name: 'app',
+    components:{
+      MyMenu
+    },
+
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  methods: {
+      OnMenuClick: function(e) {
+          alert("Click no Menu")
+      }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+h1, h2 {
+  font-weight: normal;
+}
+</style>
---
+++ src/Mymenu.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-on:click="OnMenuClick">
+        <h4>{{title}}</h4>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['title'],
+    methods: {
+        OnMenuClick: function() {
+            this.$emit('menu-click')
+        }
+    }
+}
+
+</script>
